fc40f866cfacb775e23e7f8cc38e907d1a7ff2a3
cheny <cheny@CHRISCHEN>
2020-09-23 15:06:30 +0800

    添加项目文件。

---
+++ MiddlePlatform/Controllers/MiddlePlatformController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using MiddlePlatform.Models;
+
+namespace MiddlePlatform.Controllers
+{
+    
+    public class MiddlePlatformController : ApiController
+    {
+        [Authorize]
+        [HttpGet]
+        [Route("MPAccess")]
+        public bool MPAccess(int iIn)
+        {
+            if (iIn == 0)
+                return false;
+            else
+                return true;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("MPAccess1")]
+        public int MPAccess1(int iIn)
+        {
+           return iIn;
+        }
+    }
+}
---
+++ MiddlePlatform/Models/ValueMiddlePlatform.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiddlePlatform.Models
+{
+    public class ValueMiddlePlatform
+    {
+        public bool bAccess{ get; set; }
+        public string sName { get; set; }
+
+    }
+}
